7ca13533ba15a6dd9e1e6bdd6f7bd499cd59f409
hunter-reeves <hunter.reeves@cresa.wa.gov>
2025-09-18 10:38:30 -0700

    remove extra file

--- WIN_updver.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-Command -ComputerName CES901,CES902,CES903,CES904,CES905,CES906,CES907,CES908,CES909,CES910,CES911,CES912,CES913,CES914,CES915,CES916,CES917,CES918,CES919,CES920,CES921,CES922 -ScriptBlock {Get-Hotfix} | Sort-Object PSComputerName, Description, InstalledOn -Descending
-Read-Host -Prompt "Press Enter to exit"
